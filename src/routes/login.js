e7328747422efa0e651b855abb322f4c17dc62ca
ST-AutoCanon <166044443+ST-AutoCanon@users.noreply.github.com>
2025-02-17 16:52:17 +0530

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const LoginHandler = require("../handlers/loginHandler");
-
 router.post("/login", LoginHandler.login);
+// Route to fetch salary ranges
+router.get('/salary-ranges', LoginHandler.getSalaryRanges);
+// Route for getting attendance status count
+router.get("/attendance-status", LoginHandler.getAttendanceStatusCount);
+// Route for getting login data count (Daily, Weekly, Monthly)
+router.get("/login-data-count", LoginHandler.getEmployeeLoginDataCount);
+// Route for getting employee count by department
+router.get("/employee-count", LoginHandler.getEmployeeCountByDepartment);
+router.get('/employee-count-by-department', LoginHandler.getEmployeeCountByDepartment);
+// Route for fetching total payroll data
+router.get("/total-payroll-data", LoginHandler.getEmployeePayrollData);
 
 module.exports = router;
