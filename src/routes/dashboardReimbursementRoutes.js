09d50a7f1f505974dbfff8e42732b76589927c79
ST-AutoCanon <166044443+ST-AutoCanon@users.noreply.github.com>
2025-03-10 15:07:46 +0530

    Update dashboardReimbursementRoutes.js

--- src/routes/dashboardReimbursementRoutes.js
+++ src/routes/dashboardReimbursementRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const reimbursementHandler = require("../handlers/dashboardReimbursementHandler");
+const reimbursementHandler = require("../handlers/dashboardReimbursementhandler");
 
 // Define the route to get reimbursement statistics for a specific employee
 router.get("/reimbursement/stats/:employeeId", reimbursementHandler.getReimbursementStats);
